7375339daeb03cdba231ad8bdb6a27c9ffd028a3
Erick-Machado022 <erickmachado050@gmail.com>
2026-03-11 12:42:34 -0300

    Cria formuário

---
+++ Login_fake/index.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <div>
+            <label for="nome">Usuario</label>
+            <input type="text" name="nome" placeholder="Digite seu usuario">
+        </div>
+         <form action="index.php" method="POST">
+        <div>
+            <label for="nome">Senha</label>
+            <input type="text" name="nome" placeholder="Digite sua senha">
+        </div>
+    </form>
+    </form>
+</body>
+</html>
